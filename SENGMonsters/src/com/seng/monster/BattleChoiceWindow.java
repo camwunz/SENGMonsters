b32ac51782f6421e8e6e0ee12ee37722a7a4ddb8
Cameron Wu <camwunz@gmail.com>
2022-04-05 12:06:39 +1200

    added two windows

---
+++ SENGMonsters/src/com/seng/monster/BattleChoiceWindow.java
@@ -0,0 +1,43 @@
+package com.seng.monster;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class BattleChoiceWindow {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					BattleChoiceWindow window = new BattleChoiceWindow();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public BattleChoiceWindow() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
